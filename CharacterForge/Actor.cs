b9fa0b1fc7861887ece67f6aa897e3f60987c281
James <jamesstopford@gmail.com>
2020-04-24 20:29:22 -0400

    Add project files.

---
+++ CharacterForge/Actor.cs
@@ -0,0 +1,24 @@
+﻿namespace CharacterForge
+{
+    public class Actor
+    {
+        private int _age;
+        private   string _name {  get;  set; }
+
+        public Actor()
+        {
+            GenerateName("test");
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        private void GenerateName(object actorNamingConvention)
+        {
+            this._name = "Joe";
+        }
+
+    }
+}
---
+++ CharacterForge/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CharacterForge
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var person = new Actor();
+
+            Console.WriteLine("Person's name is {0}", person.GetName());
+
+        }
+    }
+}
